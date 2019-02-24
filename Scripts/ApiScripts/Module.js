ac8f387e93ff37f5de402916f86cf9427641957f
Andre <trasbas@yahoo.com>
2019-02-24 06:55:01 +0200

    Add files via upload

---
+++ Scripts/ApiScripts/Module.js
@@ -0,0 +1,227 @@
+﻿var app;
+(function () {
+    app = angular.module("APIModule", ['ui.bootstrap']);
+
+    angular.module('APIModule').controller('DatepickerPopupDemoCtrl', function ($scope) {
+        $scope.today = function () {
+            $scope.dt = new Date();
+        };
+        $scope.today();
+
+        $scope.clear = function () {
+            $scope.dt = null;
+        };
+
+        $scope.inlineOptions = {
+            customClass: getDayClass,
+            minDate: new Date(),
+            showWeeks: true
+        };
+
+        $scope.dateOptions = {
+            dateDisabled: disabled,
+            formatYear: 'yy',
+            maxDate: new Date(2020, 5, 22),
+            minDate: new Date(),
+            startingDay: 1
+        };
+
+        // Disable weekend selection
+        function disabled(data) {
+            var date = data.date,
+                mode = data.mode;
+            return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+        }
+
+        $scope.toggleMin = function () {
+            $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+            $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+        };
+
+        $scope.toggleMin();
+
+        $scope.open1 = function () {
+            $scope.popup1.opened = true;
+        };
+
+        $scope.open2 = function () {
+            $scope.popup2.opened = true;
+        };
+
+        $scope.setDate = function (year, month, day) {
+            $scope.dt = new Date(year, month, day);
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+        $scope.format = $scope.formats[0];
+        $scope.altInputFormats = ['M!/d!/yyyy'];
+
+        $scope.popup1 = {
+            opened: false
+        };
+
+        $scope.popup2 = {
+            opened: false
+        };
+
+        var tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        var afterTomorrow = new Date();
+        afterTomorrow.setDate(tomorrow.getDate() + 1);
+        $scope.events = [
+            {
+                date: tomorrow,
+                status: 'full'
+            },
+            {
+                date: afterTomorrow,
+                status: 'partially'
+            }
+        ];
+
+        function getDayClass(data) {
+            var date = data.date,
+                mode = data.mode;
+            if (mode === 'day') {
+                var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+                for (var i = 0; i < $scope.events.length; i++) {
+                    var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+
+                    if (dayToCheck === currentDay) {
+                        return $scope.events[i].status;
+                    }
+                }
+            }
+
+            return '';
+        }
+    });
+
+    angular.module('APIModule').controller('ModalDemoCtrl', function ($uibModal, $log, $document) {
+        var $ctrl = this;
+        $ctrl.items = [];
+
+        $ctrl.animationsEnabled = true;
+
+        $ctrl.open = function (size, parentSelector) {
+            var parentElem = parentSelector ?
+                angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+            var modalInstance = $uibModal.open({
+                animation: $ctrl.animationsEnabled,
+                ariaLabelledBy: 'modal-title',
+                ariaDescribedBy: 'modal-body',
+                templateUrl: 'myModalContent.html',
+                controller: 'ModalInstanceCtrl',
+                controllerAs: '$ctrl',
+                size: size,
+                appendTo: parentElem,
+                resolve: {
+                    items: function () {
+                        return $ctrl.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $ctrl.selected = selectedItem;
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        $ctrl.openComponentModal = function () {
+            var modalInstance = $uibModal.open({
+                animation: $ctrl.animationsEnabled,
+                component: 'modalComponent',
+                resolve: {
+                    items: function () {
+                        return $ctrl.items;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $ctrl.selected = selectedItem;
+            }, function () {
+                $log.info('modal-component dismissed at: ' + new Date());
+            });
+        };
+
+        $ctrl.openMultipleModals = function () {
+            $uibModal.open({
+                animation: $ctrl.animationsEnabled,
+                ariaLabelledBy: 'modal-title-bottom',
+                ariaDescribedBy: 'modal-body-bottom',
+                templateUrl: 'stackedModal.html',
+                size: 'sm',
+                controller: function ($scope) {
+                    $scope.name = 'bottom';
+                }
+            });
+
+            $uibModal.open({
+                animation: $ctrl.animationsEnabled,
+                ariaLabelledBy: 'modal-title-top',
+                ariaDescribedBy: 'modal-body-top',
+                templateUrl: 'stackedModal.html',
+                size: 'sm',
+                controller: function ($scope) {
+                    $scope.name = 'top';
+                }
+            });
+        };
+
+        $ctrl.toggleAnimation = function () {
+            $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
+        };
+    });
+
+    // Please note that $uibModalInstance represents a modal window (instance) dependency.
+    // It is not the same as the $uibModal service used above.
+
+    angular.module('APIModule').controller('ModalInstanceCtrl', function ($uibModalInstance, items) {
+        var $ctrl = this;
+        $ctrl.items = items;
+        $ctrl.selected = {
+            item: $ctrl.items[0]
+        };
+
+        $ctrl.ok = function () {
+            $uibModalInstance.close($ctrl.selected.item);
+        };
+
+        $ctrl.cancel = function () {
+            $uibModalInstance.dismiss('cancel');
+        };
+    });
+
+    // Please note that the close and dismiss bindings are from $uibModalInstance.
+
+    angular.module('APIModule').component('modalComponent', {
+        templateUrl: 'myModalContent.html',
+        bindings: {
+            resolve: '<',
+            close: '&',
+            dismiss: '&'
+        },
+        controller: function () {
+            var $ctrl = this;
+
+            $ctrl.$onInit = function () {
+                $ctrl.items = $ctrl.resolve.items;
+                $ctrl.selected = {
+                    item: $ctrl.items[0]
+                };
+            };
+
+            $ctrl.ok = function () {
+                $ctrl.close({ $value: $ctrl.selected.item });
+            };
+
+            $ctrl.cancel = function () {
+                $ctrl.dismiss({ $value: 'cancel' });
+            };
+        }
+    });
+})();
---
+++ Scripts/ApiScripts/Service.js
@@ -0,0 +1,31 @@
+﻿app.service("APIService", function ($http) {
+    this.getSubs = function (sudID) {
+        var url = 'api/Subscriber';
+        return $http.get(url).then(function (response) {
+            console.log(response.data);
+            return response.data;
+        });
+    }
+    this.saveSubscriber = function (sub) {
+        return $http({
+            method: 'post',
+            data: sub,
+            url: 'api/Subscriber'
+        });
+    }
+    this.updateSubscriber = function (sub) {
+        return $http({
+            method: 'put',
+            data: sub,
+            url: 'api/Subscriber'
+        });
+    }
+    this.deleteSubscriber = function (subID) {
+        var url = 'api/Subscriber/' + subID;
+        return $http({
+            method: 'delete',
+            data: subID,
+            url: url
+        });
+    }
+});
