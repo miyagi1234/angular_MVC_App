4f0fa537b55f4042e7bd5be0d12c3715d3328cb8
Andre <trasbas@yahoo.com>
2019-02-24 09:47:23 +0200

    Add files via upload

--- Controllers/SubscriberController.cs
+++ Controllers/SubscriberController.cs
@@ -12,28 +12,11 @@ namespace AngularJsApp.Controllers
     public class SubscriberController : ApiController
     {
         private DBEntities myEntity = new DBEntities();
-
-        // GET: api/Subscriber
-        public IEnumerable<UserInfo> Get()
+        Dictionary<bool, Func<DBEntities,UserInfo,string>> handleInputErrors = new Dictionary<bool, Func<DBEntities,UserInfo,string>>()
         {
-            return myEntity.UserInfoes.AsEnumerable();
-        }
-
-        // GET: api/Subscriber/5
-        public IEnumerable<UserInfo> Get(int id)
-        {
-            return myEntity.UserInfoes.AsEnumerable();
-        }
-
-        // POST: api/Subscriber
-        public string Post(UserInfo obj)
-        {
-            if (ModelState.IsValid)
-            {
-                myEntity.UserInfoes.Add(obj);
-                myEntity.SaveChanges();
-            }
-            DateTime birthDate = Convert.ToDateTime(obj.BirthDate);
+            { true, (myEntity,obj) => { myEntity.UserInfoes.Add(obj);
+                                        myEntity.SaveChanges();
+                                        DateTime birthDate = Convert.ToDateTime(obj.BirthDate);
             var timeDiff = DateTime.Today.Year - birthDate.Year;
             if(birthDate > DateTime.Today.AddYears(-timeDiff)) timeDiff--;
             var age = timeDiff;
@@ -54,7 +37,38 @@ namespace AngularJsApp.Controllers
                 // send message "You Are A Person"
                 //call popup here with our message
                 return "You are a person...";
+            } } },
+            { false, (myEntity,obj) => { return "Please make sure all fields are filled in, did you leave something out ...\r\nor did you put words,dates, preferences in the wrong place ..."; } }
+        };
+
+        // GET: api/Subscriber
+        public IEnumerable<UserInfo> Get()
+        {
+            return myEntity.UserInfoes.AsEnumerable();
+        }
+
+        // GET: api/Subscriber/5
+        public IEnumerable<UserInfo> Get(int id)
+        {
+            return myEntity.UserInfoes.AsEnumerable();
+        }
+
+        // POST: api/Subscriber
+        public string Post(UserInfo obj)
+        {
+            bool is_there_data = false;
+            DateTime validDate;
+
+            if (!string.IsNullOrEmpty(obj.Name) || 
+                !string.IsNullOrEmpty(obj.Surname) || 
+                (obj.BirthDate != null && !DateTime.TryParse(obj.BirthDate.ToString(), out validDate)) || 
+                obj.CoffyPreference != null || obj.ColorPreference != null)
+            {
+                is_there_data = true;
             }
+
+            return handleInputErrors[is_there_data].Invoke(myEntity,obj);
+           
         }
 
         // PUT: api/Subscriber/5
